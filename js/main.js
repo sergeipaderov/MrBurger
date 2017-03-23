a35e34e87a1d6a51cd5885f9457f484e7333f47b
Sergei Paderov <sergeipaderov@gmail.com>
2017-03-23 16:31:43 -0300

    Map API

--- js/main.js
+++ js/main.js
@@ -1,84 +1,50 @@
-$(function () {
+/* ----- yandex-maps ----- */
 
-	var sections = $('.section'),
-		display = $('.maincontent'),
-		inScroll = false;
+$(function() {
+	ymaps.ready(init);
+    var myMap;
 
-	var scrollToSection = function (sectionEq) {
-		var position;
+    function init(){     
+        myMap = new ymaps.Map("map", {
+            center: [59.91815363876071, 30.30557799999997],
+            zoom: 11,
+            controls: []
+        });
 
-		if (inScroll || sectionEq < 0) return; // eq(-1) берет элемент с конца коллекции
 
-		inScroll = true;
+        /*var coords = [
+        	[59.94554327989287, 30.38935262114668],
+        	[59.91142323563909, 30.50024587065841],
+        	[59.88693161784606, 30.319658102103713],
+        	[59,97033574821672, 30.315194906302924],
+        ],
 
-		position = (sections.eq(sectionEq).index() * -100) + '%';
+        myCollection = new ymaps.GeoObjectCollection({}, {
+            draggable: false,
+            preset: 'islands#redIcon',
+            iconLayout: 'default#image',
+            iconImageHref: './img/icons/map-marker.svg',
+        	iconImageSize: [46, 57],
+       		iconImageOffset: [-26, -52]
 
-		sections.eq(sectionEq).addClass('active')
-			.siblings()
-			.removeClass('active');
+    	});
 
-		display.css({
-			'transform' : 'translate3d(0, ' + position + ', 0)'
-		});
-
-		setTimeout(function () {
-			inScroll = false;
-
-			$('.fixed-menu__item').eq(sectionEq)
-				.addClass('active')
-				.siblings()
-				.removeClass('active');
-
-		}, 1300);
-	}
-
-	$('.down-arrow').on('click', function(e){
-		e.preventDefault();
-
-		scrollToSection(1);
-	});
-
-	$('.fixed-menu__link, .order-link').on('click', function(e){
-		e.preventDefault();
-
-		var href = parseInt($(this).attr('href'));
-
-		scrollToSection(href);
-
-	});
-
-	$(document).on({
-		wheel : function (e) {
-			var deltaY = e.originalEvent.deltaY;
-			var activeSection = sections.filter('.active');
-			var reqSection;
-
-			if (deltaY > 0) { //скроллим вниз
-				reqSection = activeSection.next().index();
-			}
-
-			if (deltaY < 0) { //скроллим вверх
-				reqSection = activeSection.prev().index();
-			}
-
-			scrollToSection(reqSection);
-		},
-
-		keydown : function (e) {
-
-			var activeSection = sections.filter('.active');
-
-			if ($(e.target).is('textarea')) return;
-
-			switch (e.keyCode) {
-				case 38 : //кнопка вверх
-					scrollToSection( activeSection.prev().index());
-					break;
-
-				case 40 : // кнопка вниз
-					scrollToSection(activeSection.next().index());
-					break;
-			}
+    	for (var i = 0; i < coords.length; i++) {
+    	myCollection.add(new ymaps.Placemark(coords[i]));
 		}
-	});
+		*/
+
+		
+
+		var myPlacemark = new ymaps.Placemark([59.91815363876071, 30.30557799999997], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: './img/icons/map-marker.svg',
+        iconImageSize: [46, 57],
+        iconImageOffset: [-26, -52]
+    });
+
+        myMap.geoObjects.add(myPlacemark);
+
+        myMap.behaviors.disable('scrollZoom');
+    }
 });
